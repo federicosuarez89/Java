93de594788183d47594fbb29740cae7b07a73a66
Your Name <you@example.com>
2022-09-30 14:34:03 -0300

    Comenzamos con POO,creamos nuestra clase Auto con su metodo constructor Auto() y un ejemplar de clase (Renault)

---
+++ src/poo/Auto.java
@@ -0,0 +1,17 @@
+package poo;
+
+public class Auto {
+    int ruedas;
+    int largo;
+    int ancho;
+    int motor;
+    int peso;
+    //Creamos el constructor de la clase(estado inicial)
+    public Auto(){
+        ruedas=4;
+        largo=2000;
+        ancho=300;
+        motor=1600;
+        peso=500;
+    }
+}
