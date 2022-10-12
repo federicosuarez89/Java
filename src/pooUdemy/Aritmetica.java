64fcec702bf80fa8030733406ca5f10d01fe32e1
Your Name <you@example.com>
2022-10-12 19:03:09 -0300

    Creamos constructores con argumentos en Java Udemy

--- src/pooUdemy/Aritmetica.java
+++ src/pooUdemy/Aritmetica.java
@@ -1,10 +1,21 @@
 package pooUdemy;
 
 public class Aritmetica {
+    //Atributos de la clase
+    int a;
+    int b;
+
     public Aritmetica(){
+        a = 5;
+        b = 3;
         System.out.println("Ejecutando constructor vacio");
     }
-    public int sumar (int a,int b){
+    public Aritmetica(int arg1,int arg2){
+        a = arg1;
+        b = arg2;
+        System.out.println("Ejecutando constructor con 2 argumentos");
+    }
+    public int sumar (){
         int resultado = a + b;
         return resultado;
     }
--- src/pooUdemy/PruebaAritmetica.java
+++ src/pooUdemy/PruebaAritmetica.java
@@ -5,7 +5,11 @@ import pooUdemy.Aritmetica;
 public class PruebaAritmetica {
     public static void main(String[] args) {
         Aritmetica aritmetica = new Aritmetica();
-        int resultado = aritmetica.sumar(5,3);
+        int resultado = aritmetica.sumar();
         System.out.println("resultado = " + resultado);
+        //Creamos un segundo objeto
+        Aritmetica aritmetica2=new Aritmetica(4,2);
+        System.out.println("Resultado2= "+aritmetica2.sumar());
+        
     }
 }
