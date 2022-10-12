4638243235d483702be8424a982e789477598636
Your Name <you@example.com>
2022-10-12 20:26:43 -0300

    Alcance de variables en Java Udemy

--- src/pooUdemy/Aritmetica.java
+++ src/pooUdemy/Aritmetica.java
@@ -6,17 +6,23 @@ public class Aritmetica {
     int b;
 
     public Aritmetica(){
-        a = 5;
-        b = 3;
         System.out.println("Ejecutando constructor vacio");
     }
-    public Aritmetica(int arg1,int arg2){
-        a = arg1;
-        b = arg2;
+    public Aritmetica(int a,int b){
+        this.a = a;
+        this.b = b;
         System.out.println("Ejecutando constructor con 2 argumentos");
     }
     public int sumar (){
-        int resultado = a + b;
-        return resultado;
+        return this.a + this.b;
+    }
+    public int restar(){
+        return this.a - this.b;
+    }
+    public int multiplicar(){
+        return this.a * this.b;
+    }
+    public int dividir(){
+        return this.a / this.b;
     }
 }
--- src/pooUdemy/PruebaAritmetica.java
+++ src/pooUdemy/PruebaAritmetica.java
@@ -4,12 +4,31 @@ import pooUdemy.Aritmetica;
 
 public class PruebaAritmetica {
     public static void main(String[] args) {
+        //Variables locales
+        int operandoA=6;
+        int operandoB=2;
         Aritmetica aritmetica = new Aritmetica();
         int resultado = aritmetica.sumar();
         System.out.println("resultado = " + resultado);
+
         //Creamos un segundo objeto
         Aritmetica aritmetica2=new Aritmetica(4,2);
         System.out.println("Resultado2= "+aritmetica2.sumar());
-        
+
+        //Creamos un tercer objeto
+        Aritmetica aritmetica3=new Aritmetica(operandoA,operandoB);
+        System.out.println("operandoA = " + operandoA);
+        System.out.println("operandoB = " + operandoB);
+
+        //Imprimimos la suma
+        System.out.println("\nResultado suma: "+aritmetica3.sumar());
+        //Imprimimos la resta
+        System.out.println("Resultado resta: "+aritmetica3.restar());
+        //Imprimimos la multiplicacion
+        System.out.println("Resultado multiplicacion: "+aritmetica3.multiplicar());
+        //Imprimimos la division
+        System.out.println("Resultado division: "+aritmetica3.dividir());
+
+
     }
 }
