c498866b3fc273726caaadac571180690f7a9f6f
Your Name <you@example.com>
2022-10-12 20:37:46 -0300

    Creamos el proyecto caja en Java Udemy

---
+++ src/proyectoCaja/PruebaCaja.java
@@ -0,0 +1,9 @@
+package proyectoCaja;
+
+public class PruebaCaja {
+    public static void main(String[] args) {
+        Caja caja1=new Caja(3,2,6);
+        System.out.println("El volumen de la caja es: "+caja1.calculoVolumen());
+
+    }
+}
