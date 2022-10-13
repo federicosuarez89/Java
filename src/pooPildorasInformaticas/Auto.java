c16e0dd2d8505622181da02c95bbe94ef03a2f20
Your Name <you@example.com>
2022-10-12 21:54:46 -0300

    Creamos nuevos metodos getter y setter en Java (Pildoras video32)

--- src/pooPildorasInformaticas/Auto.java
+++ src/pooPildorasInformaticas/Auto.java
@@ -8,7 +8,8 @@ public class Auto {
     private int pesoPlataforma;
     private String color;
     private int pesoFinalAuto;
-    private boolean asientosCuero,aireAcondicionado;
+    private boolean asientosCuero;
+    private boolean aireAcondicionado;
     //Creamos el constructor de la clase(estado inicial)
     public Auto(){
         ruedas=4;
@@ -17,34 +18,85 @@ public class Auto {
         motor=1600;
         pesoPlataforma=500;
     }
+
     //Creamos un metodo getter,lo identificamos porque devuelve un valor
     public String dimeDatosGenerales(){
         return "La plataforma del auto tiene "+ruedas+" ruedas "+".Mide "+largo/1000+
                 " metros con un ancho de "+ancho+" cm y un peso de plataforma de "+
                 pesoPlataforma+" kg";
     }
+
+
     //Creamos un metodo setter,lo identificamos porque no retorna nada es vacio
     public void estableceColor(String colorAuto){
         color = colorAuto;
     }
-    //Creamos un getter que nos informe el color
+    //Creamos un getter que nos informe el color(devuelve valor)
     public String dimeColor(){
         return "El color del auto es: "+color;
     }
-    //Creamos un setter para los asientos
+
+
+    //Creamos un setter para los asientos(no devuelve valor)
     public  void configuraAsientos(String asientosCuero){
-        if (asientosCuero == "si"){
+        if (asientosCuero.equalsIgnoreCase("si")){
             this.asientosCuero = true;
         }else {
             this.asientosCuero = false;
         }
     }
-    //Creamos un getter
+    //Creamos un getter(devuelve valor)
     public String dimeAsientos(){
         if (asientosCuero == true){
             return "El auto tiene asientos de cuero";
         }else {
             return "El auto tiene asientos de pana";
         }
+    }
+
+
+    //Creamos un setter(no devuelve valor)
+    public void configuraAireAcondicionado(String aireAcondicionado){
+        if (aireAcondicionado.equalsIgnoreCase("si")){
+            this.aireAcondicionado=true;
+        }else {
+            this.aireAcondicionado=false;
+        }
+    }
+    //Creamos un getter(devuelve valor)
+    public String dimeAireAcondicionado(){
+        if (aireAcondicionado==true){
+            return "El auto tiene calefaccion";
+        }else {
+            return "El auto tiene aire acondicionado";
+        }
+    }
+
+
+    //Creamos un metodo getter y setter al mismo tiempo pero no es una buena practica
+    //devuelve valor
+    public String dimePesoAuto(){
+        int pesoCarroceria=500;
+        pesoFinalAuto=pesoPlataforma+pesoCarroceria;
+        if (asientosCuero==true){
+            pesoFinalAuto+=50;
+        }
+        if (aireAcondicionado==true){
+            pesoFinalAuto+=20;
+        }
+        return "El peso del auto es: "+pesoFinalAuto;
+    }
+
+
+    //Creamos un getter(devuelve valor)
+    public int precioAuto(){
+        int precioFinal=10000;
+        if (asientosCuero==true){
+            precioFinal+=2000;
+        }
+        if (aireAcondicionado==true){
+            precioFinal+=1500;
+        }
+        return precioFinal;
     }
 }
--- src/pooPildorasInformaticas/Uso_Auto.java
+++ src/pooPildorasInformaticas/Uso_Auto.java
@@ -1,16 +1,30 @@
 package pooPildorasInformaticas;
 
+import javax.swing.*;
+
 public class Uso_Auto {
     public static void main(String[] args) {
         Auto Renault=new Auto(); //Instanciamos una clase
         //Renault es un ejemplar de la clase Auto
         System.out.println(Renault.dimeDatosGenerales());
 
-        Renault.estableceColor("Rojo");
+        //Establecemos el color y lo mandamos a imprimir
+        Renault.estableceColor(JOptionPane.showInputDialog("Introduce el color del auto"));
         System.out.println(Renault.dimeColor());
 
-        Renault.configuraAsientos("si");
+        //Establecemos si tiene asientos de cuero y mandamos a imprimir
+        Renault.configuraAsientos(JOptionPane.showInputDialog("¿Tiene asientos de cuero?"));
         System.out.println(Renault.dimeAsientos());
+
+        //Establecemos si tiene aire acondicionado y mandamos a imprimir
+        Renault.configuraAireAcondicionado(JOptionPane.showInputDialog("¿Tiene aire acondicionado?"));
+        System.out.println(Renault.dimeAireAcondicionado());
+
+        //Como este metodo unifica setter y getter solo se llama de esta manera
+        System.out.println(Renault.dimePesoAuto());
+
+        //Pedimos el precio del auto
+        System.out.println("EL precio final del auto es: "+Renault.precioAuto());
 
 
     }
