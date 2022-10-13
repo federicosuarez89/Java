89793e0a527e0fbb5f69b5a926814ef24679fff5
Your Name <you@example.com>
2022-10-13 14:16:11 -0300

    Uso del modificador de acceso static en Java (Pildoras video37)

--- src/pooPildorasInformaticas/Pruebas.java
+++ src/pooPildorasInformaticas/Pruebas.java
@@ -4,24 +4,33 @@ public class Pruebas {
     public static void main(String[] args) {
         Empleados trabajador1=new Empleados("Federico");
         Empleados trabajador2=new Empleados("Leandro");
+        Empleados trabajador3=new Empleados("Antonio");
+        Empleados trabajador4=new Empleados("Maria");
         //Cambiamos la seccion del trabajador
         trabajador1.cambiaSeccion("RRHH");
         //Imprimimos los datos de los empleados
         System.out.println(trabajador1.devuelveDatos());
         System.out.println(trabajador2.devuelveDatos());
+        System.out.println(trabajador3.devuelveDatos());
+        System.out.println(trabajador4.devuelveDatos());
     }
 }
 class Empleados{
     public Empleados(String nombre){
         this.nombre=nombre;
         seccion="Administracion";
+        id=idSiguiente;
+        idSiguiente++;
     }
     public void cambiaSeccion(String seccion){
         this.seccion=seccion;
     }
     public String devuelveDatos(){
-        return "El nombre es: "+nombre+" y trabaja en la seccion de: "+seccion;
+        return "El nombre es: "+nombre+" y trabaja en la seccion de: "+seccion+" y el id: "+
+                id ;
     }
     private final String nombre;
     private String seccion;
+    private int id;
+    private static int idSiguiente=1;
 }
