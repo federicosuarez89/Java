cd2fb8d340ef4e35a241a23cafb494baa24374b8
Federico Leandro Suarez <federico.uni.89@gmail.com>
2022-12-18 23:11:51 -0300

    Vemos sobrecarga de constructores (Pildoras video 39)

--- src/pooPildorasInformaticas/pooEmpleados/UsoEmpleado.java
+++ src/pooPildorasInformaticas/pooEmpleados/UsoEmpleado.java
@@ -5,42 +5,19 @@ import java.util.GregorianCalendar;
 
 public class UsoEmpleado {
     public static void main(String[] args) {
-        /*
-        Empleado empleado1=new Empleado("Federico Suarez",85000,2020,10,19);
-        Empleado empleado2=new Empleado("Ana Lopez",95000,2021,1,18);
-        Empleado empleado3=new Empleado("Maria Martin",80000,2022,8,20);
-        empleado1.subeSueldo(5);
-        empleado2.subeSueldo(5);
-        empleado3.subeSueldo(5);
-        System.out.println("Nombre: "+empleado1.dameNombre()+" Sueldo: "+empleado1.dameSueldo()
-        +" Fecha de alta: "+empleado1.dameFechaContrato());
-        System.out.println("Nombre: "+empleado2.dameNombre()+" Sueldo: "+empleado2.dameSueldo()
-                +" Fecha de alta: "+empleado2.dameFechaContrato());
-        System.out.println("Nombre: "+empleado3.dameNombre()+" Sueldo: "+empleado3.dameSueldo()
-                +" Fecha de alta: "+empleado3.dameFechaContrato());
-         */
-        Empleado [] misEmpleados=new Empleado[3];
+        Empleado [] misEmpleados=new Empleado[4];
         misEmpleados[0]=new Empleado("Federico Suarez",85000,2020,10,19);
         misEmpleados[1]=new Empleado("Ana Lopez",95000,2021,1,18);
         misEmpleados[2]=new Empleado("Maria Martin",80000,2022,8,20);
+        misEmpleados[3]=new Empleado("Antonio Fernandez");
+
         for(Empleado e:misEmpleados){
             e.subeSueldo(5);
         }
-        /*
-        for (int i=0;i<3;i++){
-            misEmpleados[i].subeSueldo(5);
-        }
-        */
         for (Empleado e:misEmpleados) {
-            System.out.println("Nombre: "+e.dameNombre()+" Sueldo: "+e.dameSueldo()
-            +" Fecha de alta: "+e.dameFechaContrato());
+            System.out.println("Nombre: " + e.dameNombre() + " Sueldo: " + e.dameSueldo()
+                    + " Fecha de alta: " + e.dameFechaContrato());
         }
-         /*
-        for (int i=0;i<3;i++){
-            System.out.println("Nombre: "+misEmpleados[i].dameNombre()+" Sueldo: "+misEmpleados[i].dameSueldo()
-            +" Fecha de alta: "+misEmpleados[i].dameFechaContrato());
-        }
-          */
     }
 }
 //Creamos una nueva clase
@@ -51,13 +28,18 @@ class Empleado{
     private double sueldo;
     private Date altaContrato;
 
-    //Creamos el constructor de la clase
+    //Creamos un constructor de la clase
     public Empleado(String nombre,double sueldo,int agno,int mes,int dia){
         this.nombre=nombre;
         this.sueldo=sueldo;
         GregorianCalendar calendario=new GregorianCalendar(agno,mes-1,dia);
         altaContrato=calendario.getTime();
     }
+    //Creamos otro constructor para realizar sobrecarga de constructores
+    public Empleado(String nom){
+        this(nom,30000,2000,01,01);
+    }
+
     //creamos un getter (devuelve un valor)
     public String dameNombre(){
         return nombre;
