dee9e81ed0637018693b840623889615206d26e6
Federico Leandro Suarez <federico.uni.89@gmail.com>
2022-12-31 02:19:53 -0300

    Entregable sobre Encapsulamiento (OpenBootcamp)

---
+++ src/openBootcamp/encapsulacion/Persona.java
@@ -0,0 +1,36 @@
+/*
+    -Crear clase Persona.
+    -Crear variables las privadas edad, nombre y telefono de la clase Persona.
+    -Crear gets y sets de cada propiedad.
+ */
+package openBootcamp.encapsulacion;
+
+public class Persona {
+    private int edad;
+    private String nombre;
+    private int telefono;
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+}
