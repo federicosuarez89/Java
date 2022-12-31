65ee1f19b7194a7ee91c1edab9dc695b37beacc8
Federico Leandro Suarez <federico.uni.89@gmail.com>
2022-12-31 01:33:40 -0300

    Entregable sobre funciones clase Coche (OpenBootcamp)

---
+++ src/openBootcamp/ejercicioCoche/TestCoche.java
@@ -0,0 +1,10 @@
+package openBootcamp.ejercicioCoche;
+
+public class TestCoche {
+    public static void main(String[] args) {
+        Coche auto1 = new Coche();
+        auto1.sumaPuerta();
+        System.out.println("El auto tiene "+auto1.puertas+" puertas");
+        
+    }
+}
