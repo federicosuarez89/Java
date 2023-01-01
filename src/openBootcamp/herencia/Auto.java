3e3f4067d633b3a9b6b7162fba6fc2d2da30a844
Federico Leandro Suarez <federico.uni.89@gmail.com>
2023-01-01 20:07:23 -0300

    Herencia,polimorfismo,abstraccion,interfaces (OpenBootcamp)

---
+++ src/openBootcamp/herencia/Auto.java
@@ -0,0 +1,13 @@
+package openBootcamp.herencia;
+
+public class Auto extends Vehiculo{
+    @Override
+    public String getSonido(){
+        return "Soy un super sonido: "+this.sonido;
+    }
+
+    @Override
+    public void setSonido(String sonido) {
+        this.sonido = sonido;
+    }
+}
---
+++ src/openBootcamp/herencia/Test.java
@@ -0,0 +1,10 @@
+package openBootcamp.herencia;
+
+public class Test {
+    public static void main(String[] args) {
+        Auto Renault = new Auto();
+        Renault.setSonido("BRRRRRR");
+        System.out.println(Renault.getSonido());
+
+    }
+}
