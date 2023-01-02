f6efea3dfc818cd309e3bce524ec92130d0e367b
Federico Leandro Suarez <federico.uni.89@gmail.com>
2023-01-01 22:01:37 -0300

    Interfaces (OpenBootcamp)

---
+++ src/openBootcamp/interfaces/Test.java
@@ -0,0 +1,13 @@
+package openBootcamp.interfaces;
+
+public class Test {
+    public static void main(String[] args) {
+        Auto Renault = new Auto();
+        Moto Ducati = new Moto();
+        ejecutaAcelerar(Renault);
+        ejecutaAcelerar(Ducati);
+    }
+    public static void ejecutaAcelerar(Vehiculo vehiculo){
+        vehiculo.Acelerar(15);
+    }
+}
---
+++ src/openBootcamp/interfaces/Vehiculo.java
@@ -0,0 +1,6 @@
+package openBootcamp.interfaces;
+
+interface Vehiculo {
+    void Acelerar(int cuantaVelocidad);
+    void Frenar(int cuantaVelocidad);
+}
