6b63effabc60756317642f9a824fa7505b37478f
Federico Leandro Suarez <federico.uni.89@gmail.com>
2023-01-03 20:52:50 -0300

    Entregable funciones (Open Bootcamp)

---
+++ src/openBootcamp/ejercicioFuncionesIva/FuncionesIva.java
@@ -0,0 +1,19 @@
+/*
+    Para este ejercicio tendréis que crear una función que reciba un precio
+    y devuelva el precio con el IVA incluido.
+ */
+package openBootcamp.ejercicioFuncionesIva;
+
+import javax.swing.*;
+
+public class FuncionesIva {
+    public static void main(String[] args) {
+        System.out.println("El precio mas el iva es: "+funcionIva());
+    }
+    public static float funcionIva(){
+        float precio = Float.parseFloat(JOptionPane.showInputDialog("Ingrese el precio para sumarle el iva"));
+        float iva = 1.21F;
+        float resultado = precio * iva;
+        return resultado;
+    }
+}
