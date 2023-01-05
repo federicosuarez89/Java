d787dba2706766ff0cc6ad05957eb5c2f399f952
Federico Leandro Suarez <federico.uni.89@gmail.com>
2023-01-05 10:15:40 -0300

    Modificadores de acceso Public,Private,Protected y Por defecto (Pildoras video 47)

---
+++ src/pildorasinformaticas/modificadores_acceso/ModificadoresAcceso.java
@@ -0,0 +1,27 @@
+package pildorasinformaticas.modificadores_acceso;
+
+public class ModificadoresAcceso {
+
+
+    /*
+        Tenemos 4 posibilidades de modificadores de acceso:
+            *Public:
+                Se permite acceso desde cualquier lugar  
+            *Protected:
+                Se permite el acceso dentro de la misma clase,tambien a clases
+                dentro del mismo paquete, tambien a clases hijas pero no a
+                clases que esten en distinto paquete
+            *Private:
+                Se permite el acceso dentro de la misma clase.Pero no se permite
+                acceso desde clases que esten en el mismo paquete,tampoco desde
+                clases hijas y tampoco desde clases que esten en distintos
+                paquetes.
+            *Por defecto:
+                Se permite el acceso dentro de la misma clase,a otras clases
+                dentro del mismo paquete.Pero no a clases hijas y clases que
+                esten en otros paquetes.
+     */
+
+
+
+}
