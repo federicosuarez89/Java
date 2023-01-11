867e5ea4d71aefd75d7755b7371a768baa878296
Federico Leandro Suarez <federico.uni.89@gmail.com>
2023-01-11 13:57:14 -0300

    Ubicamos y damos tamanho a las ventanas con  Java swing (Pildoras video 56)

--- src/pildorasinformaticas/graficos/MiMarco.java
+++ src/pildorasinformaticas/graficos/MiMarco.java
@@ -4,8 +4,23 @@ import javax.swing.*;
 
 public class MiMarco extends JFrame {
     public MiMarco(){
-        //Establecemos el tamanho de la ventana
-        setSize(500,300);
+        //Solo establece el tamanho de la ventana
+        //setSize(500,300);
+
+        //Solo establece la ubicacion de la ventana
+        //setLocation(500,300);
+
+        //Establece posicion y tamanho de la ventana
+        setBounds(500,300,550,250);
+
+        //prohibe cambiar el tamanho de la ventana
+        //setResizable(false);
+
+        //permite extender la ventana a pantalla completa
+        //setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+        //Ponemos titulo a la ventana
+        setTitle("Ventana Fede");
     }
 
 }
