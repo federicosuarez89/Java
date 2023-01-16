7d5b76f1a18bb2d5801cdd9ca8566a722ff250fb
Federico Leandro Suarez <federico.uni.89@gmail.com>
2023-01-16 18:58:25 -0300

    Agregamos informacion a un archivo existente (Udemy)

--- src/udemy/manejo_archivos/ManejoArchivos.java
+++ src/udemy/manejo_archivos/ManejoArchivos.java
@@ -19,4 +19,44 @@ public class ManejoArchivos {
             e.printStackTrace();
         }
     }
+
+    public static void escribirArchivo(String nombreArchivo,String texto){
+        //Instanciamos un objeto de tipo File
+        File archivo = new File(nombreArchivo);
+
+        //Creamos la excepcion por si no se encuentra el archivo a escribir
+        try {
+            //Creamos el archivo
+            PrintWriter salida = new PrintWriter(archivo);
+            //Escribimos en el archivo
+            salida.println(texto);
+            //Cerramos
+            salida.close();
+            System.out.println("Se ha escrito exitosamente nuestro archivo .txt");
+
+        } catch (FileNotFoundException e) {
+            System.out.println("No se encontro el archivo para escribir");
+        }
+    }
+
+    public static void agregarInformacion(String nombreArchivo,String texto){
+        //Instanciamos un objeto de tipo File
+        File archivo = new File(nombreArchivo);
+
+        //Creamos la excepcion por si no se encuentra el archivo a escribir
+        try {
+            //Creamos el archivo
+            PrintWriter salida = new PrintWriter(new FileWriter(archivo,true));
+            //Escribimos en el archivo
+            salida.println(texto);
+            //Cerramos
+            salida.close();
+            System.out.println("Se ha escrito exitosamente nuestro archivo .txt");
+
+        } catch (FileNotFoundException e) {
+            System.out.println("No se encontro el archivo para escribir");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/udemy/manejo_archivos/TestManejoArchivos.java
+++ src/udemy/manejo_archivos/TestManejoArchivos.java
@@ -3,6 +3,8 @@ package udemy.manejo_archivos;
 public class TestManejoArchivos {
     public static void main(String[] args) {
         String nombreArchivo = "C:\\Users\\feder\\Desktop\\Federico\\Ejercicios Java\\src\\udemy\\manejo_archivos\\prueba.txt";
-        ManejoArchivos.crearArchivo(nombreArchivo);
+        //ManejoArchivos.crearArchivo(nombreArchivo);
+        ManejoArchivos.escribirArchivo(nombreArchivo,"Hola estamos escribiendo desde Java");
+        ManejoArchivos.agregarInformacion(nombreArchivo,"Estamos agregando mas informacion");
     }
 }
