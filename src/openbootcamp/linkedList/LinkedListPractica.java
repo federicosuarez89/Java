98afe3caf2545e8b3079b8b72eff27f90ae39a97
Federico Leandro Suarez <federico.uni.89@gmail.com>
2023-01-17 21:20:13 -0300

    Vemos LinkedList (OpenBootcamp)

---
+++ src/openbootcamp/linkedList/LinkedListPractica.java
@@ -0,0 +1,19 @@
+/*
+    Una LinkedList es mu similar a un ArrayList, inclusive tienen los
+    mismos metodos. La diferencia es que un ArrayList utiliza de forma
+    subyacente un array de objetos,en cambio una LinkedList utiliza una
+    estructura de datos llamada lista doblemente enlazada.
+    El ArrayList es mucho mas rapido para almacenar y buscar datos.
+    La LinkedList es mas rapida para modificar datos.
+    La LinkedList implementa los mismo metodos que Vectores y ArrayList
+ */
+
+package openbootcamp.linkedList;
+
+import java.util.LinkedList;
+
+public class LinkedListPractica {
+    public static void main(String[] args) {
+        LinkedList<String>listaEnlazada = new LinkedList<>();
+    }
+}
